d83a15a3502fe26a406a4283f17de7cfc7c11d20
Ryel Oliver <ryel.oliver@luminance.com>
2019-08-30 15:46:28 +0100

    Exclude directories from files array

--- Zip.js
+++ Zip.js
@@ -3,7 +3,9 @@ const JSZip = require('jszip');
 async function unzip (file) {
     const zip = await JSZip.loadAsync(file);
     return {
-        files: Object.keys(zip.files),
+        files: Object.entries(zip.files)
+            .filter(([ , file ]) => !file.dir)
+            .map(([ fileName ]) => fileName),
         read: async function (filePath) {
             return await zip.file(filePath).async('string');
         },
